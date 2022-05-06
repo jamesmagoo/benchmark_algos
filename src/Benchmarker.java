0eec271778d2f4a5a9f624c711f52457213108b0
James McGauran <jamesmcgauran11@gmail.com>
2022-05-06 06:58:51 +0100

    Add merge sort algorithm

--- src/Benchmarker.java
+++ src/Benchmarker.java
@@ -15,6 +15,7 @@ public class Benchmarker {
 	private static double[] selectionSortResults = new double[9];
 	private static double[] insertionSortResults = new double[9];
 	private static double[] bucketSortResults = new double[9];
+	private static double[] mergeSortResults = new double[9];
 
 	// Array Constructor
 	Benchmarker(int size) {
@@ -45,36 +46,43 @@ public class Benchmarker {
 		double average = 1.0d * sum / arr.length;
 		return average;
 	}
-
+	
 	// method to print out final benchmark results
 	public static void displayResults() {
 
 		// define format parameters for printf output
 		String headerFormat = "%12s";
 		String format = "%12.3f";
-
+		// print table header
 		System.out.printf("%-15s", "Size:");
 		for (int n = 100; n <= 25600; n *= 2) {
 			System.out.printf(headerFormat, "N=" + n);
 		}
+		// print bubble sort results
 		System.out.println();
 		System.out.printf("%-15s", "Bubble Sort:");
 		for (int i = 0; i < bubbleSortResults.length; i++) {
 			System.out.printf(format, bubbleSortResults[i]);
 		}
-		
+		// print merge sort results
+		System.out.println();
+		System.out.printf("%-15s", "Merge Sort:");
+		for (int i = 0; i < mergeSortResults.length; i++) {
+			System.out.printf(format, mergeSortResults[i]);
+		}
+		// print bucket sort results
 		System.out.println();
 		System.out.printf("%-15s", "Bucket Sort:");
 		for (int i = 0; i < bucketSortResults.length; i++) {
 			System.out.printf(format, bucketSortResults[i]);
 		}
-
+		// print selection sort results
 		System.out.println();
 		System.out.printf("%-15s", "Selection Sort:");
 		for (int i = 0; i < selectionSortResults.length; i++) {
 			System.out.printf(format, selectionSortResults[i]);
 		}
-		
+		// print insertion sort results
 		System.out.println();
 		System.out.printf("%-15s", "Insertion Sort:");
 		for (int i = 0; i < insertionSortResults.length; i++) {
@@ -84,20 +92,22 @@ public class Benchmarker {
 	}
 
 	public static void main(String[] args) {
-		// local array to store sorting times
+		// initialise local arrays to store sorting times
 		double[] bubbleSortTimes = new double[10];
 		double[] selectionSortTimes = new double[10];
 		double[] insertionSortTimes = new double[10];
 		double[] bucketSortTimes = new double[10];
-		// count for results arrays
+		double[] mergeSortTimes = new double[10];
+		// set count for results arrays
 		int count = 0;
 		// loop through increasing data sizes
 		// int n = 100; n <= 25600; n*=2
 		for (int n = 100; n <= 25600; n *= 2) {
+			
 			// Construct array for size n
 			Benchmarker b1 = new Benchmarker(n);
-			// Test Bubble sort algorithm 10 times for average
-			// System.out.println("N="+ b1.testArray.length);
+			
+			// Test each algorithm 10 times for average
 			for (int i = 0; i < 10; i++) {
 				// Regenerate values to the array
 				b1.generateRandomArray();
@@ -113,14 +123,21 @@ public class Benchmarker {
 				
 				// produce unsorted array for next test
 				b1.generateRandomArray();
+				//b1.printArray();
 				double bucketTime = TestAlgos.bucketSort(b1.testArray, n);
-			
+				//System.out.println();
+				//b1.printArray();
+				
+				// produce unsorted array for next test
+				b1.generateRandomArray();
+				double mergeTime = TestAlgos.mergeSort(b1.testArray, 0, b1.testArray.length -1);
 				
 				// append to local time array
 				bubbleSortTimes[i] = bubbleTime;
 				selectionSortTimes[i] = selectionTime;
 				insertionSortTimes[i] = insertionTime;
 				bucketSortTimes[i] = bucketTime;
+				mergeSortTimes[i] = mergeTime;
 			}
 
 			// calculate average of ten runs per size
@@ -128,12 +145,14 @@ public class Benchmarker {
 			double avgSelection = getAverage(selectionSortTimes);
 			double avgInsertion = getAverage(insertionSortTimes);
 			double avgBucket = getAverage(bucketSortTimes);
+			double avgMerge = getAverage(mergeSortTimes);
 
 			// append average to results arrays
 			Benchmarker.bubbleSortResults[count] = avgBubble;
 			Benchmarker.selectionSortResults[count] = avgSelection;
 			Benchmarker.insertionSortResults[count] = avgInsertion;
 			Benchmarker.bucketSortResults[count] = avgBucket;
+			Benchmarker.mergeSortResults[count] = avgMerge;
 			count++;
 		}
 		
--- src/TestAlgos.java
+++ src/TestAlgos.java
@@ -47,6 +47,101 @@ public class TestAlgos {
 	}
 
 	/**
+	 * Merge Sort Algorithm (Pt.1 Main Function)
+	 * 
+	 * @param arr - the array to be sorted
+	 * @param l   - the first index of the array (0)
+	 * @param r   - the last index of the array (n-1)
+	 * @return elapsedMillis - the algorithms runtime for benchmarking
+	 * @see <a href="https://www.geeksforgeeks.org/merge-sort/?ref=lbp">Merge Sort -
+	 *      Geeks For Geeks</a>
+	 */
+
+	public static double mergeSort(int arr[], int l, int r) {
+		// start timer
+		double startTime = System.nanoTime();
+
+		if (l < r) {
+			// Find the middle point
+			int m = l + (r - l) / 2;
+
+			// Sort first and second halves
+			mergeSort(arr, l, m);
+			mergeSort(arr, m + 1, r);
+
+			// Merge the sorted halves
+			merge(arr, l, m, r);
+		}
+
+		// stop timer
+		double endTime = System.nanoTime();
+		double timeElapsed = (endTime - startTime);
+		double elapsedMillis = timeElapsed / 1000000;
+		return elapsedMillis;
+	}
+
+	/**
+	 * Merge Sort Algorithm (Pt.2 - Merge Function)
+	 * 
+	 * @param arr - the sub-arrays to be merged from Merge Sort
+	 * @param l   - first index of array
+	 * @param m   - the middle point index of the array
+	 * @param r   - last index of array
+	 * @return nothing - this function is used by the main merge sort function
+	 * @see <a href="https://www.geeksforgeeks.org/merge-sort/?ref=lbp">Merge Sort -
+	 *      Geeks For Geeks</a>
+	 *
+	 */
+
+	private static void merge(int arr[], int l, int m, int r) {
+		// Find sizes of two subarrays to be merged
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		/* Create temp arrays */
+		int L[] = new int[n1];
+		int R[] = new int[n2];
+
+		/* Copy data to temp arrays */
+		for (int i = 0; i < n1; ++i)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; ++j)
+			R[j] = arr[m + 1 + j];
+
+		/* Merge the temp arrays */
+
+		// Initial indexes of first and second subarrays
+		int i = 0, j = 0;
+
+		// Initial index of merged subarray array
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		/* Copy remaining elements of L[] if any */
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		/* Copy remaining elements of R[] if any */
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	/**
 	 * Selection Sort Algorithm
 	 * 
 	 * @param testArray - the array to be sorted
@@ -119,11 +214,12 @@ public class TestAlgos {
 	/**
 	 * Bucket Sort Algorithm
 	 * 
-	 * Credit & Reference: https://www.javatpoint.com/bucket-sort-in-java
 	 * 
 	 * @param array   - the array to be sorted
 	 * @param bktSize - size of array to be sorted
 	 * @return elapsedMillis - the algorithms runtime for benchmarking
+	 * @see <a href="https://www.javatpoint.com/bucket-sort-in-java">Bucket Sort -
+	 *      Geek For Geeks</a>
 	 *
 	 */
 
@@ -134,7 +230,7 @@ public class TestAlgos {
 		// creating a list of buckets for storing lists
 		@SuppressWarnings("unchecked")
 		List<Integer>[] buckets = new List[bktSize];
-		
+
 		// Linked list with each bucket array index
 		// as there may be hash collision
 		for (int i = 0; i < bktSize; i++) {
@@ -142,7 +238,7 @@ public class TestAlgos {
 		}
 		// calculate the hash and assigns elements to the proper bucket
 		for (int num : array) {
-			buckets[hash(num, bktSize)].add(num);
+			buckets[num / bktSize].add(num);
 		}
 		// iterate over the buckets and sorts the elements
 		for (List<Integer> bucket : buckets) {
@@ -150,7 +246,7 @@ public class TestAlgos {
 			Collections.sort(bucket);
 		}
 		int index = 0;
-		
+
 		// gather the buckets after sorting
 		for (List<Integer> bucket : buckets) {
 			for (int num : bucket) {
@@ -163,11 +259,6 @@ public class TestAlgos {
 		double timeElapsed = (endTime - startTime);
 		double elapsedMillis = timeElapsed / 1000000;
 		return elapsedMillis;
-	}
-
-	// Hash function for Bucket Sort
-	private static int hash(int num, int bucketSize) {
-		return num / bucketSize;
 	}
 
 }
